9bd32d23c2fad4aeee4ac8676eaff5eb21903efa
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2017-01-16 17:01:58 -0200

    nptl: alpha: Fix Race conditions in pthread cancellation (BZ#12683)
    This patch adds the alpha modifications required for the BZ#12683.
    It basically adds the required __pthread_get_pc function
    
    The default syscall_cancel.c should be fine for alpha and GCC 6.1
    generates the following assembly for syscall_cancel.c:
    
     # 52 "../sysdeps/unix/sysv/linux/syscall_cancel.c" 1
            callsys # $0 $19 <= $16 $17 $18 $20 $21 $0 $19
     # 0 "" 2
    $LVL8:
            .set    nomacro
    $LBE3:
    $LBE2:
            .loc 1 53 0
            .set    macro
     # 53 "../sysdeps/unix/sysv/linux/syscall_cancel.c" 1
            .global __syscall_cancel_arch_end
    __syscall_cancel_arch_end:
    
    Checked against a build and make check run-built-tests=no for
    alpha-linux-gnu.
    
    	* sysdeps/unix/sysv/linux/alpha/sysdep-cancel.h (__pthread_get_pc):
    	New function.

--- sysdeps/unix/sysv/linux/alpha/sysdep-cancel.h
+++ sysdeps/unix/sysv/linux/alpha/sysdep-cancel.h
@@ -47,3 +47,9 @@ extern int __local_multiple_threads attribute_hidden;
 # define RTLD_SINGLE_THREAD_P \
   __builtin_expect (THREAD_GETMEM (THREAD_SELF, \
 				   header.multiple_threads) == 0, 1)
+
+static inline
+uintptr_t __pthread_get_pc (const struct ucontext *uc)
+{
+  return uc->uc_mcontext.sc_pc;
+}
